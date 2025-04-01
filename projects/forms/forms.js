fa2aedd5b4067a1c4e67286d9f9dc4cc0fb1252c
Diantha Nugraha <diantha3@go.byuh.edu>
2025-04-01 13:27:27 -1000

    finally again

--- projects/forms/forms.js
+++ projects/forms/forms.js
@@ -6,10 +6,6 @@ document.getElementById("theForms").addEventListener("submit", function(event) {
     const password = document.getElementById("pass").value;
     const phone = document.getElementById("phone").value;
     const birthdate = document.getElementById("birthdate").value;
-    const gender = document.querySelector('input[name="gender"]:checked')?.value || "";
-    const state = document.getElementById("state").value;
-    const year = document.querySelector('input[name="year"]:checked')?.value || "";
-    const agree = document.querySelector('input[name="agree"]').checked;
     const comments = document.getElementById("comments").value;
 
 
@@ -29,10 +25,6 @@ document.getElementById("theForms").addEventListener("submit", function(event) {
         password,
         phone,
         birthdate,
-        gender,
-        state,
-        year,
-        agree,
         comments
     };
 
@@ -45,7 +37,7 @@ document.getElementById("theForms").addEventListener("submit", function(event) {
         if (xhr.readyState === 4 && xhr.status === 200) {
             const response = JSON.parse(xhr.responseText);
             document.getElementById("message").innerHTML = response.message;
-            // document.getElementById("form").innerHTML = "";
+            document.getElementById("theForms").innerHTML = "";
         } else if (xhr.readyState === 4) {
             alert("There was an error processing your form.")
         }
